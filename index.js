e9e26c989b8fdad8a995103e2eda7dec4de95e66
Horea Porutiu <horeaporutiu@gmail.com>
2017-08-25 15:57:13 -0700

    top 5 phrases

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //set up express application
-const express = require('express');
-var cors = require('cors');
-const app = express();
-var path = require('path')
-
+var request = require('request');
+var express = require('express');
+var app = express();
+var insertToDatabase = require('./insertToDatabase');
+var credentials = require('./credentials.js');
+var path = require('path');
 // parse JSON requst body
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 //set up routes and middleware
 var router = express.Router();
 var json = bodyParser.json();
@@ -13,39 +14,23 @@ var encoded = bodyParser.urlencoded({extended: true});
 app.use(router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-router.use(cors());
-app.all('*', function(req,res,next) {
-  res.header('Access-Control-Allow-Methods', "GET,PUT,POST,DELETE, OPTIONS");
-  res.header('Access-Control-Allow-Headers', "Access-Control-Allow-Origin, Accept, Content-Type");
-  if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-});
-//import credentials from another file
-var credentials = require('./credentials.js');
-
 var https = require('https'); //needed to make our request
 var querystring = require('querystring'); // used for JSON conversion
-var username = credentials.myCredentials.username;
-var password = credentials.myCredentials.password;
+var auth = 'Basic ' + new Buffer(credentials.myCredentials.translateUsername + ':' + credentials.myCredentials.translatePassword).toString('base64');
 //for some reason, auth needs to be in base64
-var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
-
-router.get('/', cors(), function(requ, res, next){
+router.get('/', function(requ, res, next){
   res.sendFile(__dirname + '/public/index.html');
 });
 
 // if testing in postman, need to include json body parser. Encoded one is for UI
-router.post('/translates', json, encoded, function(req,resp){ //our route
-  // take in what JSON we pass into Postman, and save it
-  var postData = querystring.stringify({
+router.post('/translates', json, encoded, function(req,resp){
+  //Data from UI
+  var postData = {
       'source' : req.body.source,
       'target': req.body.target,
       'text': req.body.text
-  });
+  };
+  insertToDatabase.getPopularPhrases(req,postData);
 //pass in auth, HTTP method, and URL to make HTTP Request
   var options = {
     host: 'gateway.watsonplatform.net',
@@ -53,11 +38,11 @@ router.post('/translates', json, encoded, function(req,resp){ //our route
     method: 'POST',
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + new Buffer(username + ':' + password).toString('base64')
+        'Authorization': auth
     }
   };
 // set up our Request
-  var req = https.request(options, function(res) {
+  var watsonTranslateReq = https.request(options, function(res) {
     //call back fired when there is a chunk of data
     res.on('data', function (returnVal) {
       //send our response to the client
@@ -66,8 +51,8 @@ router.post('/translates', json, encoded, function(req,resp){ //our route
     });
   });
 //needed to convert back into JSON
-  req.write(postData);
-  req.end();
+  watsonTranslateReq.write(querystring.stringify(postData));
+  watsonTranslateReq.end();
 });
 app.listen(8080);
 // object returned as part of require call
---
+++ insertToDatabase.js
@@ -0,0 +1,89 @@
+//set up express application
+var request = require('request');
+var credentials = require('./credentials.js');
+var auth = 'Basic ' + new Buffer(credentials.myCredentials.cloudantUsername + ':' + credentials.myCredentials.cloudantPassword).toString('base64');
+module.exports = {
+  getPopularPhrases: function (req,postData) {
+    var time = Date.now();
+    var parsedWords = [];
+    var splitStr = req.body.text.split(/[ ,]+/).filter(Boolean);
+    //split the words
+    for (var i = 0; i < splitStr.length; i++){
+      parsedWords[i] = splitStr[i];
+    }
+
+    var getRevId = {
+      url: credentials.myCredentials.cloudantDatabaseUrl,
+      method: 'GET',
+      headers: {
+          'Authorization': auth
+      }
+    };
+
+    request.get(getRevId, function(err, body){
+      if (err) {
+        console.log(err);
+      }
+      var docObj = JSON.parse(body.body);
+      //update dict object with database phrases
+      var dict = docObj.index.phrases;
+      //flag to check for duplicate entries
+      var insertIntoDict = true;
+      var phrases = req.body.text;
+      //if it's first time inserting into DB
+      if(dict.length <= 0) {
+        dict.push({
+          phrase: phrases,
+          count: 1
+        });
+      }
+      else{
+        //check every phrase in the database for a match
+        for(var i=0;i<dict.length;i++){
+           if (dict[i].phrase == phrases) {
+             insertIntoDict=false;
+             dict[i].count++;
+             break;
+           }
+        }
+        //if phrase is not in DB, add it!
+        if(insertIntoDict){
+          dict.push({
+            phrase: phrases,
+            count: 1
+          });
+        }
+      }
+      //sort by descending values
+      dict.sort(function(a, b) {
+        return (b.count) - (a.count);
+      });
+
+      var lastTranslation = {
+        postData: postData,
+        parsedWords: parsedWords,
+        timestamp: time,
+        phrases: dict
+      };
+      //revision number needed for Cloudant update
+      var revisionNum = docObj._rev;
+      var insertTranslation = {
+        url: credentials.myCredentials.cloudantDatabaseUrl,
+        method: 'PUT',
+        json: {
+          '_rev': revisionNum,
+          'index':lastTranslation
+        },
+        headers: {
+            'Authorization': auth,
+            'Content-Type': 'application/json'
+        }
+      };
+      request.put(insertTranslation, function(err, body){
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+  }
+};
