9fae800efe58bcaebcc44e6eca7d33f908f0eae9
Horea Porutiu <horeaporutiu@gmail.com>
2017-09-22 16:13:20 -0700

    Watson working :D, config management set. Ready to build the STOCKBOT

---
+++ Socket/app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var socket = require('socket.io');
+var jsonfile = require('jsonfile');
+config = jsonfile.readFileSync( __dirname + '/config.json' );
+var Watson = require('./watson.js')
+
+//setup app
+var app = express();
+var server = app.listen(4001, function(){
+    console.log('listening to port 4001');
+});
+
+app.use(express.static('public'));
+
+app.use(function (req, res, next){
+    
+      req.config = config;
+    
+      next();
+    
+    });
+
+//Socket setup
+var io = socket(server);
+
+
+io.on('connection', function(socket){
+
+    console.log('made socket connection', socket.id);
+
+    socket.on('showUsername', function(name) {
+        io.emit('showUsername', name);
+    });
+
+    socket.on('translate', function(phraseToTranslate){
+
+        let watson = new Watson();        
+        watson.translate(phraseToTranslate).then(function(phrase){
+            io.emit('translate', phrase);     
+        });
+    });        
+});
--- Socket/public/chat.js
+++ Socket/public/chat.js
@@ -1,10 +1,17 @@
 // Make Connection
-var socket = io.connect("http://localhost:4000");
+var socket = io.connect('http://localhost:4001');
 
 //Query DOM 
-var nameTag = document.getElementById("name");
+var nameTag = document.getElementById('name');
+var translateTag = document.getElementById('translate');
 
 //Emit events
-socket.on("showUsername", function(name){
-    nameTag.innerHTML = "Hey " + name;
+socket.on('showUsername', function(name){
+    console.log('about to say hi');
+    nameTag.innerHTML = 'Hey ' + name;
+});
+
+socket.on('translate', function(translatedPhrase){
+    console.log('were in the socket.io transalte client ');
+    translateTag.innerHTML = 'your translated phrase is ' + translatedPhrase;
 });
